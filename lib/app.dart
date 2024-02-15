5f71c3c0bb9b7c292f80bc5b401cd68664d8d107
habibide <moushab17@.com>
2024-02-15 11:52:56 +0000

    CI/CD

--- lib/app.dart
+++ lib/app.dart
@@ -28,7 +28,7 @@ class WeatherAppView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
+    final theme = Theme.of(context).textTheme;
     return BlocBuilder<ThemeCubit, Color>(
       builder: (context, color) {
         return MaterialApp(
@@ -37,7 +37,7 @@ class WeatherAppView extends StatelessWidget {
             primaryColor: color,
             textTheme: GoogleFonts.rajdhaniTextTheme(),
             appBarTheme: AppBarTheme(
-              titleTextStyle: GoogleFonts.rajdhaniTextTheme(textTheme)
+              titleTextStyle: GoogleFonts.rajdhaniTextTheme(theme)
                   .apply(bodyColor: Colors.white)
                   .titleLarge,
             ),
