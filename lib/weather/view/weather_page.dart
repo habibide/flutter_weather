eee3dc405e6fb4fac25d315285a376055cd7cdb0
habibide <moushab17@.com>
2024-04-17 12:05:05 +0000

    Google Play publishing with codemagic.yaml

--- lib/weather/view/weather_page.dart
+++ lib/weather/view/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_weather/search/search.dart';
 import 'package:flutter_weather/settings/settings.dart';
